c8e9932bccc5e452b73c3c74625fcff74f7836f1
a.porechniy <a.porechniy@thecoders.ru>
2020-11-16 01:33:29 +0300

    add React.js

--- backend/src/routing/index.js
+++ backend/src/routing/index.js
@@ -6,11 +6,14 @@ if(typeof pool !== undefined){
     router = app => {
         app.get('/', (request, response) => {
 
+            // Create current date-time for MySQL format
             let date = new Date();
             let mysql_date = date.getFullYear() + "-" + date.getMonth() + "-" + date.getDate() + " " + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
                        
+            // Sund MySQL request
             pool.query('SHOW TABLES', (err, result) => {
                 if(err){
+                    // Logging error
                     pool.query(`INSERT INTO request_log (id, url, host, response_status, date) VALUES (null, '${request.url}', '${request.headers.host}', 400, '${mysql_date}')`);
                     console.log("Error in " + request.url);
                     response.status(400);
@@ -20,6 +23,7 @@ if(typeof pool !== undefined){
                     response.setHeader('Access-Control-Allow-Headers', 'origin, content-type, accept');
                     response.setHeader('Cache-Control', 'no-cache');
                     response.send(result);
+                    // Loggin request
                     pool.query(`INSERT INTO request_log (id, url, host, response_status, date) VALUES (null, '${request.url}', '${request.headers.host}', 200, '${mysql_date}')`);
                 }
             })            
