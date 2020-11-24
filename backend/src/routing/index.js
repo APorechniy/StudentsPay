7b04bbbaa53f41417e6683e3c255f95aee44e27b
a.porechniy <a.porechniy@thecoders.ru>
2020-11-25 00:18:35 +0300

    Add registration

--- backend/src/routing/index.js
+++ backend/src/routing/index.js
@@ -4,28 +4,9 @@ const pool = require('../db_connect/index');
 
 let router;
 if(typeof pool !== undefined){
-    router = app => {
-        app.get('/', (request, response) => {
+    router = app => { 
 
-            // Create current date-time for MySQL format
-            let date = new Date();
-            let mysql_date = date.getFullYear() + "-" + date.getMonth() + "-" + date.getDate() + " " + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
-                       
-            // Send MySQL request
-            pool.query('SHOW TABLES', (err, result) => {
-                if(err){
-                    // Logging error
-                    pool.query(`INSERT INTO request_log (id, url, host, response_status, date) VALUES (null, '${request.url}', '${request.headers.host}', 400, '${mysql_date}')`);
-                    console.log("Error in " + request.url);
-                    response.status(400);
-                    response.send(null);
-                } else {                    
-                    response.send(result);
-                    // Loggin request
-                    pool.query(`INSERT INTO request_log (id, url, host, response_status, date) VALUES (null, '${request.url}', '${request.headers.host}', 200, '${mysql_date}')`);
-                }
-            })            
-        });
+        /* Получение данных пользователя по cookie */
 
         app.get(`/user`, (request, response) => {
             let date = new Date();
@@ -46,18 +27,32 @@ if(typeof pool !== undefined){
             })     
         })
 
-        app.post('/registration', (request, response) => {
+        /* Регистрация нового пользователя */
 
+        app.post('/signup', (request, response) => {
+            let userId = 0;
 
             // Create current date-time for MySQL format
             let date = new Date();
             let mysql_date = date.getFullYear() + "-" + date.getMonth() + "-" + date.getDate() + " " + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
-
-            pool.query(`INSERT INTO users (id, name, role, start_date, login, password) VALUES (null, ${request.data.name}, ${request.data.role}, ${mysql_date}, ${request.data.login}, ${request.data.password})`, (err, result) => {
+            
+            pool.query(`INSERT INTO users (id, name, role, start_date, login, password, account_status) VALUES (null, '${request.body.name}', '${request.body.role}', '${mysql_date}', '${request.body.login}', '${request.body.password}', "НА РАССМОТРЕНИИ")`, (err, result) => {
                 if(err){
                     // Logging error
                     pool.query(`INSERT INTO request_log (id, url, host, response_status, date) VALUES (null, '${request.url}', '${request.headers.host}', 400, '${mysql_date}')`);
-                    console.log("Error in " + request.url);
+                    console.log("Error in insert" + request.url);
+                } else {                    
+                    // Loggin request
+                    pool.query(`INSERT INTO request_log (id, url, host, response_status, date) VALUES (null, '${request.url}', '${request.headers.host}', 200, '${mysql_date}')`);
+                }
+            })
+            
+            pool.query(`INSERT INTO statement (id, from_user, for_type, date_create, status) VALUES (null, '${request.body.login}', "REGISTRATION", '${mysql_date}', "НА РАССМОТРЕНИИ")`, (err, result) => {
+                if(err){
+                    // Logging error
+                    console.log(err)
+                    pool.query(`INSERT INTO request_log (id, url, host, response_status, date) VALUES (null, '${request.url}', '${request.headers.host}', 400, '${mysql_date}')`);
+                    console.log("Error in statement " + request.url);
                     response.status(400);
                     response.send(null);
                 } else {                    
@@ -68,6 +63,8 @@ if(typeof pool !== undefined){
                 }
             })
         });
+
+        /* Авторизация пользователя */
 
         app.post('/signin', (request, response) => {
             setKey = async (key) => {
@@ -110,7 +107,7 @@ if(typeof pool !== undefined){
         app.get('*', (request, response) => {
             let date = new Date();
             let mysql_date = date.getFullYear() + "-" + date.getMonth() + "-" + date.getDate() + " " + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
-            pool.query(`INSERT INTO request_log (id, url, host, response_status, date) VALUES (null, '${request.url}', '${request.headers.host}', 200, '${mysql_date}')`);
+            pool.query(`INSERT INTO request_log (id, url, host, response_status, date) VALUES (null, '${request.url}', '${request.headers.host}', 400, '${mysql_date}')`);
             response.send(null);
         })
     }
